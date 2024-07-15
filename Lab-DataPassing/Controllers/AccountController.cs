dd86fe1ebf3fb68814617c316d86bf5d8a59c4f8
kishore <kishore@WIN-10>
2024-07-15 11:58:06 +0000

    Add project files.

---
+++ Lab-DataPassing/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Lab_DataPassing.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Lab_DataPassing.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Username == "kishore" && model.Password == "123456")
+                {
+
+                    TempData["Message"] = "Welcome back";
+
+                    string strUser = JsonSerializer.Serialize(model);
+                    HttpContext.Session.SetString("User", strUser);
+
+                    return RedirectToAction("Index", "Dashboard");
+
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "invalid Cred";
+                }
+            }
+            return View();
+        }
+
+    
+    }
+}
---
+++ Lab-DataPassing/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_DataPassing.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
